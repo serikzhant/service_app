265ffc3008ba9ab031bd2ec437bee5f490979d43
Serikzhan <tlekkaliserik@icloud.com>
2024-07-25 14:10:01 +0600

    redis, cache, receivers

--- service/services/models.py
+++ service/services/models.py
@@ -1,7 +1,9 @@
 from django.core.validators import MaxValueValidator
 from django.db import models
+from django.db.models.signals import post_delete
 
 from clients.models import Client
+from services.receivers import delete_cache_total_price
 from services.tasks import set_comment, set_price
 
 
@@ -63,3 +65,14 @@ class Subscription(models.Model):
 
     def __str__(self):
         return f'{self.client} - {self.service} Plan - {self.plan}'
+
+
+    def save(self, *args, **kwargs):
+        creating = not bool(self.id)
+        result = super().save(*args, **kwargs)
+        if creating:
+            set_price.delay(self.id)
+        return result
+
+
+post_delete.connect(delete_cache_total_price, sender=Subscription)
